04949df05a8afddce7454407e6a8de95e2efb14a
Matija <kreateer@protonmail.com>
2022-10-19 04:00:20 +0200

    Added buttons to test scripted movement
    I added buttons to test moving the player character back and forth after they trigger the doggo event.
    Right now, the player only teleports between places instead of having smooth and slow movement.
    
    I also updated .gitignore to ignore '.idea', which holds a bunch of files used by JetBrains Rider IDE.

--- Assets/Scripts/Events/Tutorial/DoggoCollider.cs
+++ Assets/Scripts/Events/Tutorial/DoggoCollider.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Player;
 
 namespace Events
@@ -10,6 +11,7 @@ namespace Events
         private GameObject DoggyStyle; // collider
         private GameObject PlayerBoi; // player object
         private GameObject MC; // main camera
+        private GameObject CV;
 
         // Start is called before the first frame update
         void Start()
@@ -17,21 +19,26 @@ namespace Events
             DoggyStyle = GameObject.FindWithTag("Doggo");
             MC = GameObject.FindWithTag("MainCamera");
             PlayerBoi = GameObject.FindWithTag("Player");
+            //start with canvas disabled
+            CV = GameObject.Find("ButtonCanvas");
+            CV.SetActive(false);
         }
 
         void OnTriggerEnter2D(Collider2D PlayerBoi)
         {
             if (PlayerBoi.tag == "Player")
                 Debug.Log("<color=red>Doggo collider triggered!</color>");
-            MC.GetComponent<CameraFollow>().isFollowing = false;
-            Debug.Log("<color=red>Camera no longer following player!</color>");
-            //trigger so camera stops following player
+                MC.GetComponent<CameraFollow>().isFollowing = false;
+                Debug.Log("<color=red>Camera no longer following player!</color>");
+                //trigger so camera stops following player
 
-            PlayerBoi.GetComponent<Movement>().JustMove(false);
-            PlayerBoi.GetComponent<Movement>().spriteFlip = false;
-            Debug.Log("<color=red>Player can no longer move!</color>");
-            PauseTimer();
-            //stop player for x seconds
+                PlayerBoi.GetComponent<Movement>().JustMove(false);
+                PlayerBoi.GetComponent<Movement>().spriteFlip = false;
+                Debug.Log("<color=red>Player can no longer move!</color>");
+                //enable canvas:
+                CV.SetActive(true);
+                //PauseTimer();
+                //stop player for x seconds
         }
         private void PauseTimer() //function to call a timer for player pause effect
         {
--- Assets/Scripts/Events/Tutorial/TutorialEvents.cs
+++ Assets/Scripts/Events/Tutorial/TutorialEvents.cs
@@ -1,21 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Player;
+using UnityEngine.UIElements;
 
 namespace Events
 {
     public class TutorialEvents : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private GameObject PlayerBoi;
+        private GameObject mainCamera;
+        private GameObject CV;
+
+        void Awake()
+        {
+            PlayerBoi = GameObject.FindWithTag("Player");
+            mainCamera = GameObject.FindWithTag("MainCamera");
+            CV = GameObject.Find("ButtonCanvas");
+        }
+        
+        /* Alright, lemme explain this here:
+         
+         I put two separate functions instead of one for simplicity's sake.
+         The Buttons have an 'On Click()' function within the Editor, which
+         is tied to a GameObject and a function of that object's attached script.
+         By default, the buttons expect left mouse click as trigger.
+         This means that the buttons check for clicks on their own, but
+         they only listen to separate functions. Hence my decision.
+         
+         Tl;dr: im lazy and enjoy clicking buttons :D */
+
+        public void RunBack()
         {
 
+            //flip player then move toon back
+            CV.SetActive(false);
+            PlayerBoi.GetComponent<SpriteRenderer>().flipX = true;
+            PlayerBoi.transform.position = Vector2.MoveTowards(transform.position,
+                new Vector2(0, 0), Time.deltaTime * 5);
+            
+            //re-enable camera and player movement
+            mainCamera.GetComponent<CameraFollow>().isFollowing = true;
+            PlayerBoi.GetComponent<Movement>().JustMove(true);
+            PlayerBoi.GetComponent<Movement>().speed = 4;
+            PlayerBoi.GetComponent<Movement>().spriteFlip = true;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void RunPast()
         {
+            //flip player then move toon forward
+            CV.SetActive(false);
+            PlayerBoi.GetComponent<SpriteRenderer>().flipX = false;
+            PlayerBoi.transform.position = Vector2.MoveTowards(transform.position,
+                new Vector2(1, 0), Time.deltaTime * 5);
 
+            //re-enable camera and player movement
+            mainCamera.GetComponent<CameraFollow>().isFollowing = true;
+            PlayerBoi.GetComponent<Movement>().JustMove(true);
+            PlayerBoi.GetComponent<Movement>().speed = 4;
+            PlayerBoi.GetComponent<Movement>().spriteFlip = true;
         }
     }
 }
